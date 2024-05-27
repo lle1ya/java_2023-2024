b264b5e3036664ff5e8fbf8d46b1eb883c47c44d
lle1ya <AlexTV2@yandex.ru>
2024-05-28 01:22:36 +0500

    initial commit

--- src/lr7/Example1.java
+++ src/lr7/Example1.java
@@ -2,6 +2,6 @@ package lr7;
 
 public class Example1 {
     public static void main(String[] args) {
-        System.out.println("Привет Мир!");
+        System.out.println("Привет Мир123123!");
     }
 }
